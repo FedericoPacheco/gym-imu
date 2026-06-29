13e15c4106e38cf0c63c429d57487549c0eb844a
FedericoPacheco <fedepacheco2112@gmail.com>
2026-06-29 11:21:07 -0300

    feat(IMUSampleReader): add new more efficient method for raw reads

--- signal/signal-utils/src/signal_utils/IMUSampleReader.py
+++ signal/signal-utils/src/signal_utils/IMUSampleReader.py
@@ -5,12 +5,11 @@ import numpy as np
 
 
 class IMUSampleReader:
-    FIELDS = ["seq", "ax", "ay", "az", "roll", "pitch", "yaw"]
-
+    # Deprecated, avoid in new code, use readRaw() instead
     def read(self, inputFile: str) -> dict[str, array | dict[str, array]]:
-        filePath = Path(inputFile)
-        if not filePath.exists():
-            raise FileNotFoundError(f"Capture file not found: {filePath}")
+        filePath = self._checkFileExists(inputFile)
+        headers = ["seq", "ax", "ay", "az", "wroll", "wpitch", "wyaw"]
+        self._checkHeaders(filePath, headers)
 
         motionData = {
             "a": {"x": array("f"), "y": array("f"), "z": array("f")},
@@ -37,22 +36,35 @@ class IMUSampleReader:
 
         return motionData
 
-    def readRaw(self, inputFile: str) -> dict[str, array | dict[str, array]]:
-        return self.read(inputFile)
+    # More efficient, numpy-based
+    def readRaw(self, inputFile: str) -> tuple[
+        np.ndarray,
+        np.ndarray,
+        np.ndarray,
+    ]:
+        filePath = self._checkFileExists(inputFile)
+
+        headers = ["seq", "ax", "ay", "az", "wroll", "wpitch", "wyaw"]
+        self._checkHeaders(filePath, headers)
+
+        data = np.loadtxt(filePath, delimiter=",", skiprows=1, dtype=np.float32)
+        data = np.atleast_2d(data)
+
+        seq = data[:, 0].astype(np.uint32, copy=False)
+        a = data[:, 1:4]
+        w = data[:, 4:7]
+
+        return (seq, a, w)
 
     def readFiltered(self, inputFile: str) -> tuple[
         np.ndarray,
         np.ndarray,
         np.ndarray,
     ]:
-        filePath = Path(inputFile)
-        if not filePath.exists():
-            raise FileNotFoundError(f"Capture file not found: {filePath}")
+        filePath = self._checkFileExists(inputFile)
 
-        with open(filePath, "r", encoding="utf-8", newline="") as csvFile:
-            header = csvFile.readline().strip().split(",")
-            if header != self.FIELDS:
-                raise ValueError(f"Unexpected CSV header in {filePath}: {header}")
+        headers = ["seq", "ax", "ay", "az", "roll", "pitch", "yaw"]
+        self._checkHeaders(filePath, headers)
 
         data = np.loadtxt(filePath, delimiter=",", skiprows=1, dtype=np.float32)
         data = np.atleast_2d(data)
@@ -62,3 +74,15 @@ class IMUSampleReader:
         angle = data[:, 4:7]
 
         return (seq, a, angle)
+
+    def _checkFileExists(self, inputFile: str) -> Path:
+        filePath = Path(inputFile)
+        if not filePath.exists():
+            raise FileNotFoundError(f"Capture file not found: {filePath}")
+        return filePath
+
+    def _checkHeaders(self, filePath: Path, expectedHeaders: list[str]):
+        with open(filePath, "r", encoding="utf-8", newline="") as csvFile:
+            header = csvFile.readline().strip().split(",")
+            if header != expectedHeaders:
+                raise ValueError(f"Unexpected CSV header in {filePath}: {header}")
